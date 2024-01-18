e02d44e5f8f6dd8c6f58ae40478fd5dfc578fa9b
sholafunmi <sholafunmiadewara@gmail.com>
2024-01-18 08:41:29 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def to_uper(character):
-    if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) - 32)
-    else:
-        return ord(character)
-
-
 def uppercase(str):
-    new = ""
-    for character in str:
-        new += "%c" % to_uper(character)
-    print("{:s}".format(new))
+    for i in str:
+        if ord(i) in range(97, 122):
+            print(chr(ord(i) - 32), end="")
+        else:
+            print(i, end="")
+        print("")
+uppercase("best")
